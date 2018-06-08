3b0ac4b235588e75dfa26d6ebacdabff1752b144
XiaoMuYi <34268642+XiaoMuYi@users.noreply.github.com>
2018-06-08 18:19:11 +0800

    Update ps_v1.go

--- ps_v1.go
+++ ps_v1.go
@@ -6,53 +6,18 @@ https://github.com/prometheus/prometheus/blob/737ae60ceaebc4264358e28e377eda0618
 package main
 
 import (
-	"os"
 	"fmt"
-	"log"
+	"os"
 	"io/ioutil"
+	"log"
 	"regexp"
-	"bytes"
+	"strings"
+	"strconv"
 )
-
-const userHZ = 100
-
-type ProcStat struct {
-	PID int
-	Comm string
-	State string
-	PPID	int
-	PGRP	int
-	Session int
-	TTY 	int
-	TPGID	int
-	Flags 	int
-	MinFlt	uint
-	CMinFlt	uint
-	MajFlt	uint
-	CMajFlt uint
-	STime 	uint
-	CUTime	uint
-	CSTime	uint
-	Prioyity int
-	Nice	int
-	NumThreads int
-	StartTime	uint64
-	Vsize 	int
-	RSS		int
-}
-
-/*
-type Proc struct {
-	PID int
-}
-type Procs []Proc
-*/
-
 //获取PID路径
-func PidNum() string {
+func PidNum() []string {
 	var b= true
-	var Pid string
-	var piddata []string
+	var PidData []string
 
 	Dir, err := ioutil.ReadDir("/proc")
 	if err != nil {
@@ -63,65 +28,69 @@ func PidNum() string {
 	for _, dir := range Dir {
 		b = reg.MatchString(dir.Name())
 		if dir.IsDir() && true == b {
-			piddata = append(piddata, dir.Name())
+			PidData = append(PidData, dir.Name())
 		}
 	}
-	for _, pid := range piddata {
-		Pid = pid
-	}
-	return Pid
+
+	return PidData
 }
 
-func PidStat(Pid string) (ProcStat,error) {
-	f,err := os.Open("/proc/"+ Pid + "/"+"stat")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
+func PidStat(Pid []string)  {
+	var totalVirtualMem int64
+	var totalRSSMem int64
+	var osPageSize = os.Getpagesize()
+	var TotalScollectorMemoryMB uint64
+	for _,pid := range Pid{
+		file_status,err := os.Stat("/proc/"+ pid)
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+		start_ts := file_status.ModTime().Unix()
 
-	data,err := ioutil.ReadAll(f)
-	if err != nil {
-		return ProcStat{},err
-	}
+		stats_file,err := ioutil.ReadFile("/proc/" + pid + "/stat")
+		if err !=nil {
+			log.Fatal(err)
+			continue
+		}
+		stats := strings.Fields(string(stats_file))
+		if len(stats) < 24 {
+			err = fmt.Errorf("stats too short")
+			continue
+		}
 
-	var r = bytes.LastIndex(data,[]byte(")"))
-	var s = ProcStat{}
-	_,err = fmt.Fscan(
-		bytes.NewBuffer(data[r+2:]),
-		&s.State,
-		&s.PPID,
-		&s.PGRP,
-		&s.Session,
-		&s.TTY,
-		&s.TPGID,
-		&s.Flags,
-		&s.MinFlt,
-		&s.MajFlt,
-		&s.CMajFlt,
-		&s.STime,
-		&s.CUTime,
-		&s.CSTime,
-		&s.Nice,
-		&s.NumThreads,
-		&s.Vsize,
-		&s.RSS,
-	)
-	if err != nil {
-		return ProcStat{},err
-	}
-	fmt.Println(s)
-	return s,nil
+		user,err := strconv.ParseInt(stats[13],10,64)
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+		sys,err := strconv.ParseInt(stats[14],10,64)
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+
+		virtual,err := strconv.ParseInt(stats[22],10,64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		totalVirtualMem += virtual
+
+		rss,err := strconv.ParseInt(stats[23],10,64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if pid == string(os.Getegid()) {
+			TotalScollectorMemoryMB = uint64(rss) * uint64(osPageSize) / 1024 / 1024
+		}
+
+		totalRSSMem += rss
+		//		fmt.Printf("Pid:%v ProcStartTime:%v ProcUptime:%v User:%v Sys:%v totalVirtualMem:%v TotalScollectorMemoryMB:%v",pid,start_ts,new()-start_ts,user,sys,totalVirtualMem,TotalScollectorMemoryMB)
+		fmt.Printf("Pid:%v StartTime:%v us:%v sys:%v totalVirtualMem:%v TotalScollectorMemoryMB:%v",pid,start_ts,user,sys,totalVirtualMem,TotalScollectorMemoryMB)
+		}
+
 }
 
-func (s ProcStat) VirtualMemory() int {
-	return s.Vsize
-}
-
-func (s ProcStat) ResidentMemory() int {
-	return s.RSS * os.Getpagesize()
-}
-
-func main() {
-		fmt.Println(PidNum())
+func main()  {
 	PidStat(PidNum())
 }
